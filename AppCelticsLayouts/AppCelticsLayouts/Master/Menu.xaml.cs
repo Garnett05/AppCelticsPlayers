b5d83bcee09ecec148b88996985cee1f98f38853
Garnett05 <charlleslimamota10@hotmail.com>
2020-04-05 16:06:54 -0300

    End of some layout adjusts

--- AppCelticsLayouts/AppCelticsLayouts/Master/Menu.xaml.cs
+++ AppCelticsLayouts/AppCelticsLayouts/Master/Menu.xaml.cs
@@ -3,10 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
-using AppCelticsLayouts.Model;
 
 namespace AppCelticsLayouts.Master
 {
@@ -16,29 +14,21 @@ namespace AppCelticsLayouts.Master
         public Menu()
         {
             InitializeComponent();
-        }
-        public static Player CelticsPlayer(string name, int age, string birthDate)
-        {
-            Player player = new Player { Name = name, Age = age, BirthDate = birthDate };
-            return player;
-        }
+        }        
         private void GoCelticsPage(object sender, EventArgs args)
         {            
             Detail = new Pages.Celtics();
         }
         private void GoRondoPage(object sender, EventArgs args)
-        {
-            CelticsPlayer("Rajon Rondo", 34, "22/02/1986");
+        {            
             Detail = new Pages.Profile();
         }
         private void GoPiercePage(object sender, EventArgs args)
-        {
-            CelticsPlayer("Paul Pierce", 42, "13/10/1977");
+        {            
             Detail = new Pages.Profile();
         }
         private void GoGarnettPage(object sender, EventArgs args)
-        {
-            CelticsPlayer("Kevin Garnett", 43, "19/05/1976");
+        {            
             Detail = new Pages.Profile();
         }
     }
--- AppCelticsLayouts/AppCelticsLayouts/Pages/Profile.xaml.cs
+++ AppCelticsLayouts/AppCelticsLayouts/Pages/Profile.xaml.cs
@@ -7,17 +7,14 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using AppCelticsLayouts.Master;
-using AppCelticsLayouts.Model;
 
 namespace AppCelticsLayouts.Pages
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Profile : ContentPage
-    {
+    {        
         public Profile()
         {
-            Player play = new Player();
-            play.Name = 
             InitializeComponent();
         }
     }
