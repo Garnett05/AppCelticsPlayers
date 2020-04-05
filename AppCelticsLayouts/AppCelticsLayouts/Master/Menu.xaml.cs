fcfcb7483a002998635373b3eac13cbb8a75f48c
Garnett05 <charlleslimamota10@hotmail.com>
2020-04-04 22:01:38 -0300

    Creating the menu

--- AppCelticsLayouts/AppCelticsLayouts/Master/Menu.xaml.cs
+++ AppCelticsLayouts/AppCelticsLayouts/Master/Menu.xaml.cs
@@ -10,11 +10,19 @@ using Xamarin.Forms.Xaml;
 namespace AppCelticsLayouts.Master
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class Menu : ContentPage
+    public partial class Menu : MasterDetailPage
     {
         public Menu()
         {
             InitializeComponent();
+        }
+        private void GoCelticsPage(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Pages.Profile());
+        }
+        private void GoProfilePage(object sender, EventArgs args)
+        {
+            Navigation.PushAsync(new Pages.Celtics());
         }
     }
 }
